5d7da5901442ffda58f1532e1c46af71999a27d6
AMAN SAHU <132773168+aman9496@users.noreply.github.com>
2025-10-02 22:03:37 +0000

    dataTyes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treate all js code as newer version.
+// but noe it is automatically created new 
+
+//alter(3+3) // we are using node js , not browser
+
+console.log(3
+    +
+    3); //make code readability should be high
+
+console.log("Aman");
+
+let name ="Aman";
+let age = 21
+let isLoggedIn = false
+
+// TYPES OF DATATYPES
+  // number => 2 to power 53
+  // bigint
+  // string =>""
+  // boolean => true/false
+  // null => standalone value
+  // undefind =>
+  // symbol => unique  
+
+  //object
+
+  console.log(typeof "Aman");
+  console.log(typeof null); // object
+  console.log(typeof undefind); // undefind
+  
+  
+  
